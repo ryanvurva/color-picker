379726936998ad927016432d08f933a32acc1fb0
Ryan Vurva <ryanvurva@gmail.com>
2017-03-26 23:41:22 -0400

    didn;t get much else done.  didnt like where it was going so i scratched it.

--- public/main.js
+++ public/main.js
@@ -4,41 +4,54 @@ const main = () => {
   let satValue = 50
   let lightValue = 50
   let alphaValue = 1
+  const button = document.querySelector('div.button')
+  const statements = ['one', 'ring', 'to', 'rule', 'them', 'all', 'one', 'ring', 'to', 'find', 'them', 'one', 'ring', 'to', 'bring', 'them', 'all', 'and', 'in', 'the', 'darkness', 'bind', 'them']
+  let currentStatement = 0
+
   const hueSlider = document.querySelector('input[name=hue]')
-  const hueNumber = document.querySelector('div.huenumber')
+  // const hueNumber = document.querySelector('div.huenumber')
   hueSlider.addEventListener('input', (event) => {
     hueValue = event.target.value
-    hueNumber.textContent = hueValue
+    // hueNumber.textContent = hueValue
     updateColor()
   })
 
   const satSlider = document.querySelector('input[name=saturation]')
-  const satNumber = document.querySelector('div.satnumber')
+  // const satNumber = document.querySelector('div.satnumber')
   satSlider.addEventListener('input', (event) => {
     satValue = event.target.value
-    satNumber.textContent = satValue + '%'
+    // satNumber.textContent = satValue + '%'
     updateColor()
   })
 
   const litSlider = document.querySelector('input[name=lightness]')
-  const litNumber = document.querySelector('div.litnumber')
+  // const litNumber = document.querySelector('div.litnumber')
   litSlider.addEventListener('input', (event) => {
     lightValue = event.target.value
-    litNumber.textContent = lightValue + '%'
+    // litNumber.textContent = lightValue + '%'
     updateColor()
   })
 
   const alphaSlider = document.querySelector('input[name=alpha]')
-  const alphaNumber = document.querySelector('div.alphanumber')
+  // const alphaNumber = document.querySelector('div.alphanumber')
   alphaSlider.addEventListener('input', (event) => {
-    alphaValue = event.target.value
-    alphaNumber.textContent = alphaValue
+    alphaValue = event.target.value / 100
+    // alphaNumber.textContent = alphaValue
     updateColor()
   })
 
   const updateColor = () => {
-    let color = `hsla(${hueValue}, ${satValue}%, ${lightValue}%, ${alphaValue})`
+    let color
+    if (alphaValue < 1) {
+      color = `hsla(${hueValue},   ${satValue}%,  ${lightValue}%,  ${alphaValue})`
+    } else {
+      color = `hsl(${hueValue}, ${satValue}%, ${lightValue}%)`
+    }
     colorbox.style.backgroundColor = color
+    button.textContent = color
+
+    console.log(statements[currentStatement % statements.length])
+    currentStatement += 1
   }
 }
 
